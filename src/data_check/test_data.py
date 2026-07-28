13e6591e8a05bd0ebe750b79a54a5afb549df818
saveligaj <saveligaj@gmail.com>
2026-07-28 04:57:52 +0000

    Implement data_check step and test

--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -87,3 +87,21 @@ def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_th
 ########################################################
 # Implement here test_row_count and test_price_range   #
 ########################################################
+
+def test_row_count(data: pd.DataFrame) -> None:
+    """Test that the dataset size is reasonable (not too small, not too large).
+
+    Args:
+        data: Input DataFrame to test
+    """
+    assert 15000 < data.shape[0] < 1000000
+
+def test_price_range(data: pd.DataFrame, min_price: float, max_price: float) -> None:
+    """Test that all prices fall within the expected min/max range.
+
+    Args:
+        data: Input DataFrame to test
+        min_price: Minimum allowed price
+        max_price: Maximum allowed price
+    """
+    assert data['price'].between(min_price, max_price).all()
